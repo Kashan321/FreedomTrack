75c5a8b6d216d86d90ea53e188e8c5f49c67d6b1
unknown <humayunwahla55@gmail.com>
2025-02-03 17:14:48 +0500

    add icons i navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FaTh, FaFunnelDollar, FaCog, FaBell, FaFilter } from 'react-icons/fa';
 import logo from '../../assets/logo.jpeg';
+import { CiSearch } from "react-icons/ci";
 
 function Navbar() {
   return (
-    <div className="bg-gray-700 text-white p-4 flex justify-between items-center w-full">
+    <div className=" text-white p-4 flex justify-between items-center w-full">
       <div className="flex items-center space-x-4">
         {/* Logo and Company Title */}
         <div className="flex items-center space-x-2">
@@ -14,21 +15,18 @@ function Navbar() {
       </div>
 
       {/* Search Bar */}
-      <div className="flex-1 hidden sm:flex justify-center px-4">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-full max-w-md p-2 rounded bg-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
+     
 
       {/* User Profile */}
-      <div className="flex items-center space-x-2">
-        <div className="flex items-center p-2 hover:bg-gray-600 rounded">
-          <FaBell className="mr-2" />
+      <div className="flex items-center space-x-5">
+      <div className=" w-8 h-8 bg-gray-400 flex justify-center items-center  rounded-full">
+          <CiSearch  className="" />
+        </div>
+        <div className=" w-8 h-8 bg-gray-400 flex justify-center items-center rounded-full">
+          <FaBell className="" />
         </div>
         <img className="rounded-full w-10 h-10" src="profile-pic-url" alt="Profile" />
-        <span>Username</span>
+        
       </div>
     </div>
   );
